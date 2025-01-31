ff1c170ba56aecd582c33811bb99a506455a3183
smtmfft <smtm@taiko.xyz>
2025-02-01 01:34:24 +0800

    use batch json rpc

--- pythons/eth_trace_block.py
+++ pythons/eth_trace_block.py
@@ -9,85 +9,90 @@ import os
 
 app = Flask(__name__)
 
+
 class HexJsonEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, HexBytes):
             return obj.hex()
         return super().default(obj)
 
+
 def get_block_data(w3: Web3, block_number: int):
     # block info
     block = w3.eth.get_block(block_number, full_transactions=True)
-    
+
     # collect addresses & slots
     addresses = set()
     storage_slots = defaultdict(set)
     contracts = {}
 
-    # trace tx
-    for tx in block.transactions:
-        app.logger.debug(f"trace tx: {tx}")
-        trace = w3.provider.make_request(
-            "debug_traceTransaction",
-            ["0x"+tx.hash.hex(), {"tracer": "prestateTracer"}]
-        )
-
-        app.logger.debug(f"trace: {trace}")
-        
-        if 'result' in trace:
-            result = trace['result']
-            addresses.update(result.keys())
-            for addr, info in result.items():
-                checksum_addr = Web3.to_checksum_address(addr)
-                app.logger.debug(checksum_addr)
-                if 'storage' in info:
-                    storage_slots[checksum_addr].update(info['storage'].keys())
-                if 'code' in info and info['code'] != '0x':
-                    contracts['0x'+w3.keccak(hexstr=checksum_addr).hex()] = info['code']
+    proof_requests = []
+    # trace block txs
+    trace = w3.provider.make_request(
+        "debug_traceBlockByNumber", [hex(block_number), {"tracer": "prestateTracer"}]
+    )
+    app.logger.debug(f"trace: {trace}")
+    if "result" in trace:
+        for tx_trace in trace["result"]:
+            for addr, info in tx_trace["result"].items():
+                addresses.add(addr)
+                if "storage" in info:
+                    storage_slots[addr].update(info["storage"].keys())
+                # collect contracts
+                if "code" in info and info["code"] != "0x":
+                    code_hash = w3.keccak(hexstr=addr)
+                    contracts["0x" + code_hash.hex()] = info["code"]
 
     app.logger.debug(f"touched addresses: {addresses}")
-    app.logger.debug(f"storage_slots: {storage_slots}")
+    app.logger.debug(f"storage slots: {storage_slots}")
+    app.logger.info(f"contract keys: {contracts.keys()}")
+
+    proof_requests = []
+    for address in addresses:
+        slots = list(storage_slots[address])
+        # pre state proof
+        proof_requests.append(("eth_getProof", [address, slots, hex(block_number - 1)]))
+        # post state proof
+        proof_requests.append(("eth_getProof", [address, slots, hex(block_number)]))
+
+    # batch rpc all
+    batch_provider = w3.provider
+    responses = batch_provider.make_batch_request(proof_requests)
+
+    app.logger.debug(f"batch_provider eth_getProof responses: {responses}")
     # proofs
     pre_proofs = []
     post_proofs = []
-    for address in addresses:
-        checksum_addr = Web3.to_checksum_address(address)        
-        slots = list(storage_slots[checksum_addr])
-        # get pre state (N-1 block) proofs
-        pre_proof = w3.eth.get_proof(
-            checksum_addr,
-            slots,
-            block_number - 1
-        )
-        pre_proofs.append(pre_proof)
-        
-        # post state (N block) proofs
-        post_proof = w3.eth.get_proof(
-            checksum_addr,
-            slots,
-            block_number
-        )
-        post_proofs.append(post_proof)
+    for i, response in enumerate(responses):
+        if i % 2 == 0:
+            pre_proofs.append(serialize_web3_data(response["result"]))
+        else:
+            post_proofs.append(serialize_web3_data(response["result"]))
 
     # ancestors
     ancestor_hashes = []
     current_number = block_number - 1
+    proof_requests = []
     for _ in range(256):  # to support block.hash opcode
         if current_number < 0:
             break
-        ancestor_block = w3.eth.get_block(current_number)
-        ancestor_hashes.append(ancestor_block.hash)
+        proof_requests.append(("eth_getBlockByNumber", [hex(current_number), False]))
+
         current_number -= 1
 
-    app.logger.debug(f"list contracts keys: {contracts.keys()}")
+    responses = batch_provider.make_batch_request(proof_requests)
+    app.logger.debug(f"batch_provider eth_getBlockByNumber responses: {responses}")
+    for response in responses:
+        ancestor_hashes.append(response["result"]["hash"])
 
     return {
         "block": block,
         "preAccountProofs": pre_proofs,
         "postAccountProofs": post_proofs,
         "contracts": contracts,
-        "ancestorHashes": ancestor_hashes
+        "ancestorHashes": ancestor_hashes,
     }
+
 
 class Web3Encoder(json.JSONEncoder):
     def default(self, obj):
@@ -98,7 +103,9 @@ class Web3Encoder(json.JSONEncoder):
         # TODO: more types
         return json.JSONEncoder.default(self, obj)
 
+
 app.json_encoder = Web3Encoder
+
 
 def serialize_web3_data(obj):
     if isinstance(obj, dict):
@@ -106,22 +113,23 @@ def serialize_web3_data(obj):
     elif isinstance(obj, list):
         return [serialize_web3_data(x) for x in obj]
     elif isinstance(obj, HexBytes):
-        return "0x"+obj.hex()
+        return "0x" + obj.hex()
     elif isinstance(obj, bytes):
-        return "0x"+eth_utils.to_hex(obj)
-    elif hasattr(obj, 'hex'):
-        return "0x"+obj.hex()
-    elif hasattr(obj, '__dict__'):
+        return to_hex(obj)
+    elif hasattr(obj, "hex"):
+        return "0x" + obj.hex()
+    elif hasattr(obj, "__dict__"):
         return serialize_web3_data(obj.__dict__)
     return obj
 
 
 rpc_url = os.getenv("TAIKO_DEBUG_RPC")
 
-#TODO: real lru
+# TODO: real lru
 lru_cache = defaultdict(set)
 
-@app.route('/trace/<int:block_number>')
+
+@app.route("/trace/<int:block_number>")
 def trace_block(block_number):
     app.logger.info(f"Received request for block {block_number}")
 
@@ -132,7 +140,7 @@ def trace_block(block_number):
         result = get_block_data(w3, block_number)
         # app.logger.debug(f"----------------------------------------------------------")
         # app.logger.debug(f"result: {result}")
-        # app.logger.debug(f"----------------------------------------------------------")        
+        # app.logger.debug(f"----------------------------------------------------------")
         # response = jsonify(result)
         serialized_data = serialize_web3_data(dict(result))
         # app.logger.debug(f"serialized_data: {serialized_data}")
@@ -147,7 +155,8 @@ def trace_block(block_number):
     finally:
         app.logger.info("Request completed")
 
-app.logger.setLevel(logging.DEBUG)
-if __name__ == '__main__':
+
+app.logger.setLevel(logging.INFO)
+if __name__ == "__main__":
     app.logger.info("Starting server on port 8090")
-    app.run(host='0.0.0.0', port=8090, debug=True)
+    app.run(host="0.0.0.0", port=8090, debug=True)
